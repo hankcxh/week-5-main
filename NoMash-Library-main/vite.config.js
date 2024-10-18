337221ca0d22630cd16366759ead8ac0c74a37b6
Hank <xche0302@student.monash.edu>
2024-10-18 20:32:15 +1100

    week12 fixed changes problem

--- NoMash-Library-main/vite.config.js
+++ NoMash-Library-main/vite.config.js
@@ -4,11 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/<week-5-main>/'
-    : '/'
-}
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
